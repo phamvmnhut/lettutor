975f061e12823f102e334bc53a510edff63b674d
phamvmnhut <phamvmnhut@gmail.com>
2022-01-22 09:39:18 +0700

    tutor_detail: play video

--- lib/ui/tutor_detail/comp/intro_tutor_video.dart
+++ lib/ui/tutor_detail/comp/intro_tutor_video.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+
 import 'package:lettutor/models/tutor_detail.dart';
 
-class IntroTutorVideo extends StatelessWidget {
+class IntroTutorVideo extends StatefulWidget {
   IntroTutorVideo({Key? key, required this.data}) : super(key: key);
   final TutorDetailModel data;
 
   @override
+  _IntroTutorVideoState createState() => _IntroTutorVideoState();
+}
+
+class _IntroTutorVideoState extends State<IntroTutorVideo> {
+  late VideoPlayerController videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerController = VideoPlayerController.network(widget.data.video);
+    videoPlayerController.initialize();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+    );
     return Center(
       child: Container(
         height: 200,
         width: double.infinity,
-        color: Colors.redAccent,
+        child: Chewie(
+          controller: chewieController,
+        ),
       ),
     );
   }
